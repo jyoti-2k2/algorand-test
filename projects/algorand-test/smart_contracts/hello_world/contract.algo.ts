dccd281c655a03c059a2efd74d11312e40956aa5
Jyoti Kumari <jyotik.2k2@gmail.com>
2025-09-06 15:22:33 +0530

    add digital signature storge smart contract

--- projects/algorand-test/smart_contracts/hello_world/contract.algo.ts
+++ projects/algorand-test/smart_contracts/hello_world/contract.algo.ts
@@ -1,7 +1,45 @@
-import { Contract } from '@algorandfoundation/algorand-typescript'
+import { Contract, GlobalState } from "@algorandfoundation/algorand-typescript";
 
-export class HelloWorld extends Contract {
-  hello(name: string): string {
-    return `Hello, ${name}`
+export class DigitalSignatureStorage extends Contract {
+
+  // Store a signature in global state
+  signature = GlobalState<string>({
+    key: "signature",
+    initialValue: "",
+  });
+
+  // Store the signer (address)
+  signer = GlobalState<string>({
+    key: "signer",
+    initialValue: "",
+  });
+
+  /**
+   * Save a digital signature with the signer's address
+   * @param signerAddress Address of the person signing
+   * @param signatureData Digital signature string (e.g., hash of document)
+   * @returns The stored signature
+   */
+  storeSignature(signerAddress: string, signatureData: string): string {
+    this.signer.value = signerAddress;
+    this.signature.value = signatureData;
+    return signatureData;
+  }
+
+  /**
+   * Retrieve the stored signature
+   * @returns The stored signature
+   */
+  getSignature(): string {
+    return this.signature.value;
+  }
+
+  /**
+   * Retrieve the signer’s address
+   * @returns The signer address
+   */
+  getSigner(): string {
+    return this.signer.value;
   }
 }
+
